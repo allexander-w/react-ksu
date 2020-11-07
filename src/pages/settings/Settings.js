a4381819fccc8db22681a0c64e2d4d2d477cc1a2
alexander-w <work.1793593@gmail.com>
2020-11-07 18:12:53 +0600

    Settings page fix

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -3,8 +3,13 @@ import React from 'react'
 export const Settings = props => {
     return (
         <div>Settings
-            <p>Hello, world!</p>
+            <p>Hello, world! ASd</p>
             <p>Hello, Earth!</p>
+
+
+            <div>
+                Very cool
+            </div>
         </div>
     )
 }
