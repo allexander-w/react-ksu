43d0504347cb6c3c1a8ea6778b37519148c60cb3
Ertaev <darhanertaev10@gmail.com>
2020-11-07 19:56:09 +0600

    name icons

--- src/components/main/Sidebar.js
+++ src/components/main/Sidebar.js
@@ -2,7 +2,35 @@ import React from 'react'
 import './Sidebar.css'
 
 export const Sidebar = props => {
-
+/*  sidebar: [
+    {
+      path: '/',
+      title: 'Информация',
+      icon: 'far fa-receipt',
+      exact: true
+    },
+    {
+      path: '/studentbook',
+      title: 'Зачетка',
+      icon: 'far fa-book'
+    },
+    {
+      path: '#',
+      title: 'ИУП',
+      icon: 'far fa-clipboard-list'
+    },
+    {
+      path: '#',
+      title: 'Оплата',
+      icon: 'far fa-money-check-alt'
+    },
+    {
+      path: '#',
+      title: 'Е-Обходной',
+      icon: 'far fa-laptop'
+    }
+  ]
+})*/
 
   return (
     <div className='sidebar'>
