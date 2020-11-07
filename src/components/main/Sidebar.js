fa5516d99bf4f2b875236abb453313315f05d1be
alexander-w <work.1793593@gmail.com>
2020-11-07 19:58:35 +0600

    Sidebar

--- src/components/main/Sidebar.js
+++ src/components/main/Sidebar.js
@@ -7,6 +7,12 @@ export const Sidebar = props => {
   return (
     <div className='sidebar'>
       Sidebarr
+
+      <ul>
+        <li className='sidebar-item'>
+          as
+        </li>
+      </ul>
     </div>
   )
 }
